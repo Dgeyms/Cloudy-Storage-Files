3eeded97ebce22ea3710d23fc73cfe05a7727865
Dgeyms <dgeyms@gmail.com>
2024-07-01 13:48:52 +0300

    feat(project): add class AuthController.java

---
+++ src/main/java/com/example/cloudystoragefiles/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.example.cloudystoragefiles.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+    @PostMapping("/registerUser")
+    public String registerUser(User user){
+
+        return "";
+    }
+
+
+}
