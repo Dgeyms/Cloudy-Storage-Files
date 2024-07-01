36550ac7c8643ea90098416845d68be1e8e2a7e4
Dgeyms <dgeyms@gmail.com>
2024-07-01 14:12:39 +0300

    feat(project): add class User.java

---
+++ src/main/java/com/example/cloudystoragefiles/model/User.java
@@ -0,0 +1,41 @@
+package com.example.cloudystoragefiles.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
