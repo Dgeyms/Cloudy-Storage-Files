2591bc9c71debf7596676db468a1bc8bfee9dd2b
Dgeyms <dgeyms@gmail.com>
2024-07-02 14:18:54 +0300

    fix(project): rename method registerUser in registerUserAccount. And chang in method.

--- src/main/java/com/example/cloudystoragefiles/controller/AuthController.java
+++ src/main/java/com/example/cloudystoragefiles/controller/AuthController.java
@@ -1,11 +1,15 @@
 package com.example.cloudystoragefiles.controller;
 
-import com.example.cloudystoragefiles.model.User;
+import com.example.cloudystoragefiles.model.UserDTO;
 import com.example.cloudystoragefiles.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AuthController {
@@ -18,19 +22,28 @@ public class AuthController {
     }
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
     }
 
     @GetMapping("/register")
-    public String register(){
+    public String register() {
         return "register";
     }
 
-    @PostMapping("/registerUser")
-    public String registerUser(User user){
-        userService.saveUserInDatabase(user);
-        return "redirect:/login?success";
+    @PostMapping("/user/registration")
+    public ModelAndView registerUserAccount(
+            @ModelAttribute("user") @Valid UserDTO userDTO,
+            HttpServletRequest request,
+            Error error) {
+
+        try {
+            User registered = userService.registerNewUserAccount(userDTO);
+        } catch (UserAlreadyExistException uaeEx) {
+            mav.addObject("message", "An account for that username already exists.");
+            return mav;
+        }
+        return new ModelAndView("successRegister", "user", userDTO);
     }
 
 
